89912873d49d497df5a7aa34e6251b1e70318397
ryanmueller28 <ryan.mueller@doane.edu>
2020-05-13 15:58:12 -0500

    Updated with a stronger and more common code structure in "cleaning-up-code"

---
+++ cleaning-up-code/Window.h
@@ -0,0 +1,102 @@
+#pragma once
+
+#include "stdio.h"
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+class Window
+{
+public:
+	Window();
+
+	Window(GLint windowWidth, GLint windowHeight);
+
+	int Initialise();
+
+	GLint getBufferWidth() { return bufferWidth; }
+	GLint getBufferHeight() { return bufferHeight; }
+
+	bool getShouldClose() { return glfwWindowShouldClose(mainWindow); }
+
+	void swapBuffers() { glfwSwapBuffers(mainWindow); }
+
+	~Window();
+
+private:
+	GLFWwindow* mainWindow;
+
+	GLint width, height;
+	GLint bufferWidth, bufferHeight;
+};
+
+
+Window::Window()
+{
+	width = 800;
+	height = 600;
+}
+
+Window::Window(GLint windowWidth, GLint windowHeight)
+{
+	width = windowWidth;
+	height = windowHeight;
+}
+
+int Window::Initialise()
+{
+	if (!glfwInit())
+	{
+		printf("Error Initialising GLFW");
+		glfwTerminate();
+		return 1;
+	}
+
+	// Setup GLFW Windows Properties
+	// OpenGL version
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// Core Profile
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	// Allow forward compatiblity
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	// Create the window
+	mainWindow = glfwCreateWindow(width, height, "Test Window", NULL, NULL);
+	if (!mainWindow)
+	{
+		printf("Error creating GLFW window!");
+		glfwTerminate();
+		return 1;
+	}
+
+	// Get buffer size information
+	glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
+
+	// Set the current context
+	glfwMakeContextCurrent(mainWindow);
+
+	// Allow modern extension access
+	glewExperimental = GL_TRUE;
+
+	GLenum error = glewInit();
+	if (error != GLEW_OK)
+	{
+		printf("Error: %s", glewGetErrorString(error));
+		glfwDestroyWindow(mainWindow);
+		glfwTerminate();
+		return 1;
+	}
+
+	glEnable(GL_DEPTH_TEST);
+
+	// Create Viewport
+	glViewport(0, 0, bufferWidth, bufferHeight);
+}
+
+
+Window::~Window()
+{
+	glfwDestroyWindow(mainWindow);
+	glfwTerminate();
+}
